41b77be03b715afb5e9c0178a22f9cd106c5c837
evanadwyer <evanadwyer@pronoid-software.com>
2024-09-17 13:51:49 -0700

    [Hard] Assignment #3
    You decide to make a simple mobile application to simulate the birds’ sounds you hear in the morning. After planning, you’ve decided on a single screen with three buttons. Each button will show the bird's name on the screen and print its sound to the console. Tapping a new button will replace the previous bird's name on the screen, and only the new bird’s sounds will print to the console.
    
    Instructions
    
    Create a single-screen app using coroutines in Compose. Add three buttons, each representing a different bird. Create a composable function for each bird that displays the bird’s name and launches a coroutine to print the bird's sound to the console. Only render the composable of the bird represented by the last button tapped and ensure only the selected bird's sounds are printed.

--- app/src/main/java/com/pronoidsoftware/kotlincoroutinesandflowsmasterclass/MainActivity.kt
+++ app/src/main/java/com/pronoidsoftware/kotlincoroutinesandflowsmasterclass/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pronoidsoftware.kotlincoroutinesandflowsmasterclass.ui.theme.KotlinCoroutinesAndFlowsMasterclassTheme
@@ -22,37 +31,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val birds = GlobalScope.launch {
-            val bird1 = launch {
-                while (true) {
-                    println("Coo")
-                    delay(1000L)
-                }
-            }
-            val bird2 = launch {
-                while (true) {
-                    println("Caw")
-                    delay(2000L)
-                }
-            }
-            val bird3 = launch {
-                while (true) {
-                    println("Chirp")
-                    delay(3000L)
-                }
-            }
-            delay(10000L)
-            bird1.cancel()
-            bird2.cancel()
-            bird3.cancel()
-        }
-
         setContent {
             KotlinCoroutinesAndFlowsMasterclassTheme {
+                var selectedBird by remember {
+                    mutableStateOf<Bird>(Bird.Bird1)
+                }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.fillMaxSize().padding(innerPadding))
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.SpaceAround,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(selectedBird.name)
+                        Button(onClick = { selectedBird = Bird.Bird1 }) {
+                            Text(Bird.Bird1.name)
+                        }
+                        Button(onClick = { selectedBird = Bird.Bird2 }) {
+                            Text(Bird.Bird2.name)
+                        }
+                        Button(onClick = { selectedBird = Bird.Bird3 }) {
+                            Text(Bird.Bird3.name)
+                        }
+                        SelectedBird(selectedBird)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun SelectedBird(
+    selectedBird: Bird
+) {
+    LaunchedEffect(selectedBird.name) {
+        while (true) {
+            println(selectedBird.song)
+            delay(selectedBird.frequency)
+        }
+    }
+}
+
+sealed class Bird(
+    val name: String,
+    val song: String,
+    val frequency: Long
+) {
+    data object Bird1 : Bird("Bird1", "Coo", 1000L)
+    data object Bird2 : Bird("Bird2", "Caw", 2000L)
+    data object Bird3 : Bird("Bird3", "Chirp", 3000L)
 }
