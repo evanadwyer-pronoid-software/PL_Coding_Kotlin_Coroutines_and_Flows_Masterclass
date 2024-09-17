bd8ad58dbeab1dd8ebe74193169aee8c04d050d5
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-09-17 16:10:01 -0700

    [Easy] Assignment #1 (#6)
    You’ve created a file documenting your thoughts on your bird friends and want to write a short application to upload it to a server for safekeeping. You write up a small demo project before integrating the API and notice a bug…
    
    Instructions
    
    Find and fix the bug in the code below to terminate the program when it has been canceled.

---
+++ app/src/main/java/com/pronoidsoftware/kotlincoroutinesandflowsmasterclass/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.pronoidsoftware.kotlincoroutinesandflowsmasterclass
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+class MyApplication: Application() {
+    val applicationScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+}
